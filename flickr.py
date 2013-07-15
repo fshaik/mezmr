991b8d697becaf6ca51b31845de8798577e0cd76
Faisal Shaik <faisalshaik@gmail.com>
2013-07-14 22:09:13 -0700

    a commit

--- flickr.py
+++ flickr.py
@@ -1,42 +1,80 @@
 import flickrapi
-import urllib
+import urllib 
 import sys
+import subprocess
+import time
 
-api_key = 'f523daf2713c00c0d82ff776c66655c6'
-flickr = flickrapi.FlickrAPI(api_key)
+slideshowtimeinsecs = 3
 
+class Downloader:
 
-def getten(theme, picnum):
-  if (theme == 'Landscape'):
-    gid = '364847@N20'
+  def __init__(self):
+    self.api_key = 'f523daf2713c00c0d82ff776c66655c6'
+    self.flickr = flickrapi.FlickrAPI(self.api_key)
+    self.imagefilename = 0
 
-  pool = flickr.groups_pools_getPhotos( api_key=api_key, group_id=gid, page='1', per_page=str(picnum))
+  def getten(self, theme, picnum,resultpagenumber):
+    if (theme == 'Landscape'):
+      gid = '364847@N20'
+
+    pool = self.flickr.groups_pools_getPhotos( api_key=self.api_key, group_id=gid, page=str(resultpagenumber), per_page=str(picnum))
   
-  photoid = []
-  secretid = []
-  serverid = []
-  farmid = []
+    photoid = []
+    secretid = []
+    serverid = []
+    farmid = []
   
-  for photo in pool.iter('photo'):
-    photoid.append(photo.attrib['id']) 
-    secretid.append(photo.attrib['secret'])
-    serverid.append(photo.attrib['server'])
-    farmid.append(photo.attrib['farm'])
+    for photo in pool.iter('photo'):
+      photoid.append(photo.attrib['id']) 
+      secretid.append(photo.attrib['secret'])
+      serverid.append(photo.attrib['server'])
+      farmid.append(photo.attrib['farm'])
   
-  photourl = []
+    photourl = []
   
-  for i in range(len(farmid)):
-    url = "http://farm%s.staticflickr.com/%s/%s_%s.jpg" % (farmid[i], serverid[i], photoid[i], secretid[i])
-    save(url, 'Landscape', i)
+    for i in range(len(farmid)):
+      url = "http://farm%s.staticflickr.com/%s/%s_%s_b.jpg" % (farmid[i], serverid[i], photoid[i], secretid[i])
+      print url
+      self.save(url, 'Landscape', self.imagefilename)
+      self.imagefilename+=1
 
-def save(link, theme, num):
-  #Save in folder
-  image = urllib.urlretrieve(str(link), "images/" + str(num) + ".jpg")
+  def save(self, link, theme, num):
+    #Save in folder
+    image = urllib.urlretrieve(str(link), "images/" + str(num) + ".jpg")
 
 
-def main(picload):
-  getten('Landscape',picload)
+def main( numofpicstoload):
+  initial = Downloader()
+  resultpagenumber = 1
+  
+  initial.getten('Landscape',numofpicstoload, resultpagenumber)
+  resultpagenumber = 2
+  fbihandler = subprocess.Popen('fbi -noverbose -a -t 3 ./images/*.jpg', shell=True)
+
+
+  while fbihandler.poll() == None: #return code could be negative
+    time.sleep(int(numofpicstoload)/2 * slideshowtimeinsecs) #chillout
+    print "Downloading More Images"
+    initial.getten('Landscape', numofpicstoload, resultpagenumber)
+    resultpagenumber += 1
+    if resultpagenumber % 3:
+      inital.imagefilename = 0
+
+
+  fbihandler.wait()
+
+ #getten and name them differently
+#clean up every three times
+
+  
+
+
+  #resultpagenumber = resultpagenumber + 1
+  #initial.getten('Landscape',picload, resultpagenumber)
+    #pause
+    #getten for next page
   
 if __name__ == '__main__':
-  
+  assert(sys.argv[1])
+
   main(sys.argv[1])
